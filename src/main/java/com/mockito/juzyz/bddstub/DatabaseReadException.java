737a975280094302e028f91780a142ea550dd057
Iuliia Rudi <connection454@gmail.com>
2024-01-08 09:58:23 -0500

    add DatabaseReadException

---
+++ src/main/java/com/mockito/juzyz/bddstub/DatabaseReadException.java
@@ -0,0 +1,7 @@
+package com.mockito.juzyz.bddstub;
+
+public class DatabaseReadException extends RuntimeException {
+    public DatabaseReadException(String message) {
+        super(message);
+    }
+}
