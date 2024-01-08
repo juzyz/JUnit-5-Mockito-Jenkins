92ccff59c5e71836c804a674ac13718c90ad2d8d
Iuliia Rudi <connection454@gmail.com>
2024-01-08 09:59:01 -0500

    add DatabaseWriteException

---
+++ src/main/java/com/mockito/juzyz/mockfeatures/exceptions/DatabaseWriteException.java
@@ -0,0 +1,7 @@
+package com.mockito.juzyz.mockfeatures.exceptions;
+
+public class DatabaseWriteException extends Throwable {
+    public DatabaseWriteException(String message) {
+        super(message);
+    }
+}
