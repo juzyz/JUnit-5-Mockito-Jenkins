d6fdefc433429545c1cdfad12a2db6975f461d7f
Iuliia Rudi <connection454@gmail.com>
2024-03-05 23:41:59 -0500

    added main test class for JUnit

---
+++ src/test/java/com/springboot/juzyz/JUnit5MockitoJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.juzyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnit5MockitoJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
