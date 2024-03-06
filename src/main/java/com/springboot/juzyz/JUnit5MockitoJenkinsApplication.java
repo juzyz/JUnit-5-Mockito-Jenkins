435ad6177269f294cb6fb2fd370fb0a1104a5b82
Iuliia Rudi <connection454@gmail.com>
2024-03-05 23:41:36 -0500

    added main class for JUnit

---
+++ src/main/java/com/springboot/juzyz/JUnit5MockitoJenkinsApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.juzyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnit5MockitoJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnit5MockitoJenkinsApplication.class, args);
+	}
+}
