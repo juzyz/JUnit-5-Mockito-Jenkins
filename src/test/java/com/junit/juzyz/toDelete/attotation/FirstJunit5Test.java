7a7b3d9bef13cc039b86b9e310359be63e7bebfe
Iuliia Rudi <connection454@gmail.com>
2024-03-21 17:44:37 -0400

    added StringHelper for JUnit

---
+++ src/test/java/com/junit/juzyz/toDelete/attotation/FirstJunit5Test.java
@@ -0,0 +1,13 @@
+package com.junit.juzyz.toDelete.attotation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+public class FirstJunit5Test {
+    @Test
+    void test () {
+//        fail("Not implemented yet");
+    }
+}
